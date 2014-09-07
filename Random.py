5cabdd06b1878cdc27b31a594142c2082e6796bd
Tigran Hakobyan <tik.hakobyan@gmail.com>
2014-09-07 19:39:13 -0400

    Added merge on 2 sorted arrays.

--- Random.py
+++ Random.py
@@ -17,9 +17,52 @@ def decToBinary(n):
 		
 	ret.insert(0,1)
 	return ret
+
+# My name is Tigran" --> "Tigran is name my"
+# Will be imlemented in place and recursive
+# def reverseWordsInPlace(str):
+# 	pass
+
+"""
+[1, 2, 3], [4, 5, 6, 7] ==> [1, 2, 3, 4, 5, 6, 7]
+
+"""
+def mergeSortedLists(lst1,lst2):
+	#let's check if one of them is empty
+	if (len(lst1) == 0):
+		 return lst2
+	if (len(lst2) == 0):
+		 return lst1
+	
+	i = 0
+	j = 0
+	# idealy we want to have len(lst1) + len(lst2) size list
+	resultList = []
+	while (i < len(lst1) and j < len(lst2)):
+		if (lst1[i] < lst2[j]):
+			resultList.append(lst1[i])
+			i+=1
+		else:
+			resultList.append(lst2[j])
+			j+=1
+			
+	while i < len(lst1):
+		resultList.append(lst1[i])
+		i+=1
+	while j < len(lst2):
+		resultList.append(lst2[j])
+		j+=1
+		
+	return resultList
+	# there might be some leftover elements in lists so let's
+	# check it out. 
+		 
 	
 def main():
-	n = int(input("n: "))
-	print(decToBinary(n))
+	# n = int(input("n: "))
+	a = [1,2,3,11,18,25]
+	b = [4,5,6,7,12,19,24]
+	print(mergeSortedLists(a,b))
+
 
 main()
