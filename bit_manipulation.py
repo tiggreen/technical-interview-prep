c982ccae9fa32830f8bbdf6913b4adae8ef3b51a
Tigran Hakobyan <tiggreen@users.noreply.github.com>
2015-12-10 09:24:04 -0500

    Refactor bit manipulation problems.

--- bit_manipulation.py
+++ bit_manipulation.py
@@ -1,39 +1,68 @@
+# Bit Manipulation problems
 
-# Return the i-th bit of num.
-def getBit(num, i):
+# x << y
+# Returns x with the bits shifted to the left by y places (and new bits on the right-hand-side are zeros).
+#  This is the same as multiplying x by 2**y.
+#
+# x >> y
+# Returns x with the bits shifted to the right by y places. This is the same as //'ing x by 2**y.
+#
+# x & y
+# Does a "bitwise and". Each bit of the output is 1 if the corresponding bit of x AND of y is 1, otherwise it's 0.
+#
+# x | y
+# Does a "bitwise or". Each bit of the output is 0 if the corresponding bit of x AND of y is 0, otherwise it's 1.
+#
+# ~ x
+# Returns the complement of x - the number you get by switching each 1 for a 0 and each 0
+# for a 1. This is the same as -x - 1.
+#
+# x ^ y
+# Does a "bitwise exclusive or". Each bit of the output is the same as the corresponding bit
+# in x if that bit in y is 0, and it's the complement of the bit in x if that bit in y is 1.
+#
+# Source: https://wiki.python.org/moin/BitwiseOperators
+
+def get_bit(num, i):
+	"""return the i-th bit of num"""
 	return (num & (1 << i) != 0)
 
-# Sets the i-th bit of num.
-def setBit(num, i):
+def set_bit(num, i):
+	"""set the i-th bit of num."""
 	return (num | (1 << i))
 
-"""
-You are given two 32-bit numbers, N and M, and two bit positions, 
-i and j. Write a method to set all bits between i and j in N equal 
-to M (e.g., M becomes a substring of N located at i and starting at j).
-EXAMPLE:
-Input: N = 10000000000, M = 10101, i = 2, j = 6
-Output: N = 10001010100
-"""
 
-def updateBits(n, m, i, j):
+# You are given two 32-bit numbers, N and M, and two bit positions, 
+# i and j. Write a method to set all bits between i and j in N equal 
+# to M (e.g., M becomes a substring of N located at i and starting at j).
+# EXAMPLE:
+# Input: N = 10000000000, M = 10101, i = 2, j = 6
+# Output: N = 10001010100
+def update_bits(n, m, i, j):
 	mx = ~0 # all 1's
-	
 	#1's through position j then 0's
 	left = mx - ((1 << j) - 1)
-	
 	#1's after position i
 	right = ((1 << i) -1)
-	
 	#1's, with 0's between i and j.
 	mask = left | right
-	
 	# clear i through j then put m in there
-	
 	return (n & mask) | (m << i)
-	
-		
-def main():
-	print(bin(updateBits(16, 16, 0,5)))
 
-main()
+def is_power_of_2(n):
+	"""
+	Check if given n is power of 2 or not. Like 2, 8, 16, 256
+	"""
+	# ~ is the compliment of n
+	return (n & ~n) == 0
+
+def main():
+	print(is_power_of_2(16) == True)
+	print(is_power_of_2(256) == True)
+	print(is_power_of_2(2) == True)
+	print(is_power_of_2(7) == False)
+	print(is_power_of_2(156) == False)
+
+
+if __name__ == '__main__':
+	main()
--- moderate_and_hard.py
+++ moderate_and_hard.py
@@ -1,78 +1,74 @@
 # @tiggreen
-# Some random type of problems. 
+# Some random moderate and hard problems.
 
-"""
-	Converts the decimal (10 based) number to a binary number
-	decToBinary(8) => 1000
-"""
+
 def decToBinary(n):
-	ret = list()
-	while (n > 1):
+    """
+    Converts the decimal (10 based) number to a binary number
+    decToBinary(8) => 1000
+    """
+    ret = list()
+    while (n > 1):
+        if n % 2 == 0:
+            ret.insert(0,0)
+        else:
+            ret.insert(0,1)
+        n = n//2
 
-		if n % 2 == 0:
-			ret.insert(0,0)
-		else:
-			ret.insert(0,1)
-		n = n//2
-		
-	ret.insert(0,1)
-	return ret
+    ret.insert(0,1)
+    return ret
 
-# My name is Tigran" --> "Tigran is name my"
-# Will be imlemented in place and recursive
-def reverseWordsInPlace(str):
-	if len(str.split()) == 1:
-		return str
-	return str.split()[-1] + " " + reverseWordsInPlace(' '.join(str.split()[:-1]))
+def reverse_words_in_place(str):
+    if len(str.split()) == 1:
+        return str
+    return str.split()[-1] + " " + reverseWordsInPlace(' '.join(str.split()[:-1]))
 
-"""
-[1, 2, 3], [4, 5, 6, 7] ==> [1, 2, 3, 4, 5, 6, 7]
 
-"""
+# [1, 2, 3], [4, 5, 6, 7] ==> [1, 2, 3, 4, 5, 6, 7]
 def mergeSortedLists(lst1,lst2):
-	#let's check if one of them is empty
-	if (len(lst1) == 0):
-		 return lst2
-	if (len(lst2) == 0):
-		 return lst1
-	
-	i = 0
-	j = 0
-	# idealy we want to have len(lst1) + len(lst2) size list
-	resultList = []
-	while (i < len(lst1) and j < len(lst2)):
-		if (lst1[i] < lst2[j]):
-			resultList.append(lst1[i])
-			i+=1
-		else:
-			resultList.append(lst2[j])
-			j+=1
-			
-	while i < len(lst1):
-		resultList.append(lst1[i])
-		i+=1
-	while j < len(lst2):
-		resultList.append(lst2[j])
-		j+=1
-		
-	return resultList
-	# there might be some leftover elements in lists so let's
-	# check it out. 
-	
-# let's try to find the data in the given list.	 
+    #let's check if one of them is empty
+    if (len(lst1) == 0):
+         return lst2
+    if (len(lst2) == 0):
+         return lst1
+
+    i = 0
+    j = 0
+    # idealy we want to have len(lst1) + len(lst2) size list
+    resultList = []
+    while (i < len(lst1) and j < len(lst2)):
+        if (lst1[i] < lst2[j]):
+            resultList.append(lst1[i])
+            i+=1
+        else:
+            resultList.append(lst2[j])
+            j+=1
+
+    while i < len(lst1):
+        resultList.append(lst1[i])
+        i+=1
+    while j < len(lst2):
+        resultList.append(lst2[j])
+        j+=1
+
+    return resultList
+    # there might be some leftover elements in lists so let's
+    # check it out. 
+    
+# let's try to find the data in the given list.  
 # Returns true if we can find the data.
 def binarySearchRec(lst, data):
-	if lst == []:
-		return False
-		
-	midInd = len(lst)//2
-	
-	if lst[midInd] == data:
-		return True
-	if data < lst[midInd]:
-		return binarySearchRec(lst[:midInd], data)
-	elif data > lst[midInd]:
-		return binarySearchRec(lst[midInd+1:], data)
+    if lst == []:
+        return False
+        
+    midInd = len(lst)//2
+    
+    if lst[midInd] == data:
+        return True
+    if data < lst[midInd]:
+        return binarySearchRec(lst[:midInd], data)
+    elif data > lst[midInd]:
+        return binarySearchRec(lst[midInd+1:], data)
 
 """
 Returns all the permutations of the string.
@@ -80,39 +76,39 @@ E.g. "abc" => "abc", "acb", "bac", "bca", "cab", "cba"
 """
 
 def getAllPermutations(st):
-	perms = []
-	if st == None:
-		return st
-	# the base case. 
-	if len(st) == 0:
-		perms.append('')
-		return perms
-	else:
-		first = st[0]
-		remainder = st[1:]
+    perms = []
+    if st == None:
+        return st
+    # the base case. 
+    if len(st) == 0:
+        perms.append('')
+        return perms
+    else:
+        first = st[0]
+        remainder = st[1:]
 
-		words = getAllPermutations(remainder)
-		for w in words:
-			for i in range(len(w)+1):
-				perms.append(insertCharAt(w, first,i))
-		return perms
+        words = getAllPermutations(remainder)
+        for w in words:
+            for i in range(len(w)+1):
+                perms.append(insertCharAt(w, first,i))
+        return perms
 
 def insertCharAt(st, f, pos):
-	result = st[:pos] + f + st[pos:]
-	return result
+    result = st[:pos] + f + st[pos:]
+    return result
 
 # ls = [a,b] => [[], a, b, [a,b]]
 def getPowerSet(ls):
-	allSubsets = []
-	sz = 1 << len(ls)
-	for counter in range(sz):
-		tempSubset  = []
-		for j in range(len(ls)):
-			if (counter & 1 << j) > 0:
-				tempSubset.append(ls[j])
-		allSubsets.append(tempSubset)
-	return allSubsets
-	
+    allSubsets = []
+    sz = 1 << len(ls)
+    for counter in range(sz):
+        tempSubset  = []
+        for j in range(len(ls)):
+            if (counter & 1 << j) > 0:
+                tempSubset.append(ls[j])
+        allSubsets.append(tempSubset)
+    return allSubsets
+    
 """
 Finding pair of numbers in a list that add to given sum.
 a = [1,3,4,6,12,16,19, 10]
@@ -120,119 +116,118 @@ findPairSum(a, 28) => (12,16)
 key : sum - x, value y
 """
 def findPairSum(lst, sm):
-	if not lst:
-		return lst
-	d = dict()
-	result = []
-	for i in lst:
-		if i not in d:
-			d[sm - i] = i
-		else:
-			key = sm - i
-			result.append((key,i))
-			
-	return result
+    if not lst:
+        return lst
+    d = dict()
+    result = []
+    for i in lst:
+        if i not in d:
+            d[sm - i] = i
+        else:
+            key = sm - i
+            result.append((key,i))
+            
+    return result
 
 def getAllPerms(st):
-	perms = []
-	
-	if st == '':
-		perms.append('')
-		return perms 
-	
-	firstChars = st[:-1]
-	lastChar = st[-1]
-	allPerms = getAllPerms(firstChars)
-	
-	for s in allPerms:
-		for i in range(len(s) + 1):
-			temp = insertCharAt(s, lastChar, i)
-			perms.append(temp)
-	print(perms)			
-	return perms
+    perms = []
+    
+    if st == '':
+        perms.append('')
+        return perms 
+    
+    firstChars = st[:-1]
+    lastChar = st[-1]
+    allPerms = getAllPerms(firstChars)
+    
+    for s in allPerms:
+        for i in range(len(s) + 1):
+            temp = insertCharAt(s, lastChar, i)
+            perms.append(temp)
+    print(perms)            
+    return perms
 
 # Print all valid combinations of n-pairs of parentheses.
 def printAllParens(cnt):
-	st = ['' for i in range(cnt*2)]
-	return printAllParensUtil(cnt, cnt, 0, st)
-	
+    st = ['' for i in range(cnt*2)]
+    return printAllParensUtil(cnt, cnt, 0, st)
+    
 def printAllParensUtil(left, right, cnt, st):
-	
-	if left < 0 or left > right:
-		return 
-	if left == 0 and right == 0:
-		print(st)
-	else:
-		if left > 0:
-			st[cnt] = '('
-			printAllParensUtil(left-1, right, cnt+1, st)
-			
-		if right > left:
-			st[cnt] = ')'
-			printAllParensUtil(left, right-1, cnt+1, st)
+    
+    if left < 0 or left > right:
+        return 
+    if left == 0 and right == 0:
+        print(st)
+    else:
+        if left > 0:
+            st[cnt] = '('
+            printAllParensUtil(left-1, right, cnt+1, st)
+            
+        if right > left:
+            st[cnt] = ')'
+            printAllParensUtil(left, right-1, cnt+1, st)
 
 #Calculate x raised to the power n */
 def pow(x,n):
-	if n == 0:
-		return 1
-	elif n % 2 == 0:
-		return pow(x, n//2)*pow(x, n//2)
-	else:
-		return x*pow(x, n//2)*pow(x, n//2)
-		
+    if n == 0:
+        return 1
+    elif n % 2 == 0:
+        return pow(x, n//2)*pow(x, n//2)
+    else:
+        return x*pow(x, n//2)*pow(x, n//2)
+        
 """
 Given an input string and a dictionary of words, find out if
 the input string can be segmented into a space-separated sequence of 
 dictionary words. See following examples for more details.
 """
 def wordBreak(st, dic):
-	#base case
-	if len(st) == 0:
-		return True
-	for i in range(1,len(st) + 1):
-		if st[0:i] in dic and wordBreak(st[i:len(st)-i], dic):
-			return True
-	return False
+    #base case
+    if len(st) == 0:
+        return True
+    for i in range(1,len(st) + 1):
+        if st[0:i] in dic and wordBreak(st[i:len(st)-i], dic):
+            return True
+    return False
 
 """
 "abcdbfa" => "cdbfa" (do this in place)
 Q: Do we want to remove first occurance chars or the second, or next?
 """
 def removeDuplicates(st):
-	if st == None:
-		return None
-	# let this be the base case
-	if st == "":
-		return st
-		
-	head = st[0]
-	tail = st[1:]
-	
-	if head in tail:
-		return removeDuplicates(tail)
-	else:
-		return head + removeDuplicates(tail)
-		
+    if st == None:
+        return None
+    # let this be the base case
+    if st == "":
+        return st
+
+    head = st[0]
+    tail = st[1:]
+    if head in tail:
+        return removeDuplicates(tail)
+    else:
+        return head + removeDuplicates(tail)
+
 def reverseString(st):
-	if st is None:
-		return None
-	if st == "":
-		return st
-	return st[-1] + reverseString(st[:-1])
+    if st is None:
+        return None
+    if st == "":
+        return st
+    return st[-1] + reverseString(st[:-1])
 
 # Reverse the digits of the integer
 # 12345 -> 54321
 
 def reverseDigits(num):
-	newnum  = 0
-	while num > 0:
-		digit = num % 10
-		num = num // 10
-		newnum = newnum*10 + digit
-	return newnum		
-	
+    newnum  = 0
+    while num > 0:
+        digit = num % 10
+        num = num // 10
+        newnum = newnum*10 + digit
+    return newnum       
+    
 def main():
-	print(reverseDigits(1))
-	
+    print(reverseDigits(1))
+    
 if __name__ == '__main__':
-	main()
+    main()
